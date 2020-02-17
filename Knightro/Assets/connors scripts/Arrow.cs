d04c10cde4493a03bbc1a5f49ea9008b882d94f7
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-02-17 23:33:30 +0000

    arrow

---
+++ Knightro/Assets/connors scripts/Arrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public GameObject CurrentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 m = CurrentObject.transform.position;
+        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.RotateTowards(m), 1f);
+        transform.LookAt(CurrentObject.transform);
+    }
+}
