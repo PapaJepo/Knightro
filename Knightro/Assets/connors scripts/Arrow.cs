a44459df2b1097a2afec55b9ff4fbc78364f190e
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-02-18 16:40:17 +0000

    Waypoint

--- Knightro/Assets/connors scripts/Arrow.cs
+++ Knightro/Assets/connors scripts/Arrow.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public GameObject CurrentObject;
+    private int CurrentWaypoint;
+    //public GameObject CurrentObject;
+    public List<Transform> Waypoints;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CurrentWaypoint = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 m = CurrentObject.transform.position;
+        UpdateWaypoints();
+        //Vector3 m = CurrentObject.transform.position;
         // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.RotateTowards(m), 1f);
-        transform.LookAt(CurrentObject.transform);
+        transform.LookAt(Waypoints[CurrentWaypoint]);
+    }
+
+    void UpdateWaypoints()
+    {
+        if(Vector3.Distance(Waypoints[CurrentWaypoint].position,this.transform.position) < 10)
+        {
+            CurrentWaypoint = (CurrentWaypoint + 1)%Waypoints.Count;
+        }
     }
 }
